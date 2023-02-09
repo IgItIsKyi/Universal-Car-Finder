921d4c9d7c869083b6c9a364ceea76f8f3e9e54f
Icedragon78 <johan.sarmiento99@Knights.ucf.edu>
2023-02-09 14:25:42 -0500

    calculator
    Worked on loan calculator

--- main.js
+++ main.js
@@ -1,11 +1,17 @@
-var Loan_Term = document.getElementById('Loan-Term');
-var Interest_Rate = document.getElementById('Interest-Rate');
-var Down_Payment = document.getElementById('Down-Payment');
-var Sales_Tax = document.getElementById('Sales-Tax');
-var result = document.getElementById('result');
+let Principal = document.getElementById('Principal');
+let Loan_Term = document.getElementById('Loan-Term');
+let Interest_Rate = document.getElementById('Interest-Rate');
+let Down_Payment = document.getElementById('Down-Payment');
+let Sales_Tax = document.getElementById('Sales-Tax');
+let result = document.getElementById('result');
+let n = 12;
+let Monthly_pay = 0;
 
 document.getElementById('Calculate').addEventListener("click", 
-function(){
-    res.value = parseInt(Loan_Term.value) +parseInt(Interest_Rate.value) 
-    + parseInt(Down_Payment.value) + parseInt(Sales_Tax.value)
+function()
+{ 
+    Monthly_pay = parseFloat(Principal.value) * ( (parseFloat(Interest_Rate.value) / 100) / n ) 
+    / (1 - (1 + (parseFloat(Interest_Rate.value) / 100) / n )**(-n * (parseFloat(Loan_Term.value)/n)) )
+    
+    document.getElementById("result").innerHTML =  Monthly_pay
 });
